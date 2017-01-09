fe304677e02ec0baff9ff7fae238c22d9df6d929
Asif Junaid <asif.junaid@nsiglobal.com>
2017-01-09 15:11:42 +0400

    routing functions

---
+++ app/config/routes.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+
+var routes = (
+    <Router history={hashHistory}>
+        <Route path='/' component={Main}>
+            <IndexRoute component={Home} />
+        </Route>
+    </Router>
+);
+module.exports = routes;
+
+            // <Route path='/playerOne' header='playerOne' component={PropmptContainer} />
+            // <Route path='/playerTwo/:playerOne' header='playerTwo' component={PropmptContainer} />
+            // <Route path='/battle' component={ConfirmBattleContainer} />
+            // <Route path='results' component={ResultsContainer} />
--- app/index.js
+++ app/index.js
@@ -1,47 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var USER_DATA = {
-    name: 'Asif Junaid',
-    username: 'asif13',
-    image: 'https://avatars2.githubusercontent.com/u/3802254?v=3&s=460'
-}
-var ProfilePic = React.createClass({
-    render: function(){
-        return <img src={this.props.imageUrl} style={{height: 100,width: 100}} />
-    }
-})
-var ProfileLink = React.createClass({
-    render: function(){
-        return (
-            <div>
-                <a href={'https://github.com/' + this.props.username}>
-                    {this.props.username}
-                </a>
-            </div>
-        )
-    }
-})
-var ProfileName = React.createClass({
-    render: function(){
-        return(
-            <div>{this.props.name}</div>
-        )
-    }
-})
-
-var Avatar = React.createClass({
-    render: function(){
-        return(
-            <div>
-                <ProfilePic imageUrl={this.props.user.image} />
-                <ProfileName name={this.props.user.name} />
-                <ProfileLink username={this.props.user.username} />
-            </div>
-        )
-    }
-})
-
+var routes = require('./config/routes'); 
 ReactDOM.render(
-    <Avatar user={USER_DATA} />,
+    routes,
     document.getElementById('app')
 );

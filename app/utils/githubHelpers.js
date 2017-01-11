dc6e43ce580d5e9b6866e467fe6f6e8d87434c9a
Asif Junaid <asif.junaid@nsiglobal.com>
2017-01-11 10:04:22 +0400

    helper issue fixed

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -2,22 +2,21 @@ var axios = require('axios');
 var id = "";
 var sec = "";
 var param = "?client_id"+id+"&client_secret="+sec;
-function getuserInfo(username){
-    return axios.get('https://api.github.com/users/'+username)
+function getUserInfo (username) {
+  return axios.get('https://api.github.com/users/' + username);
 }
 var helpers = {
-    getPlayersInfo: function(players){
-        return axios.all(players.map(function(username){
-            return getuserInfo(username);
-        })).then(function(info){
-            return info.map(function(user){
-                return user.data;
-            })
-        })
-    }).catch(function(err){
-        console.warn('Error in getPlayersInfo',err);
+  getPlayersInfo: function (players) {
+    return axios.all(players.map(function (username) {
+      return getUserInfo(username)
+    }))
+    .then(function (info) {
+      return info.map(function (user) {
+        return user.data
+      })
     })
-    }
+    .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
+  }
 };
 
 module.exports = helpers;

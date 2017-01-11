4167f16e5179bf19b7a52ba8c1458afe05d5b7b5
Asif Junaid <asif.junaid@nsiglobal.com>
2017-01-11 10:15:57 +0400

    parsing data

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,7 +1,12 @@
 var React = require('react');
 
+function puke(Object){
+    return(
+        <pre>{JSON.stringify(Object,null,' ')}</pre>
+    )
+};
 function ConfirmBattle(props) {
-    return props.isLoading  === true ? <p>Loading...</p>:<p>Confirm battle</p>
+    return props.isLoading  === true ? <p>Loading...</p>:<p>Confirm battle : {puke(props)}</p>
 };
 
 module.exports = ConfirmBattle;
